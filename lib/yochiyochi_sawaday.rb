553540c1f9e5eab24ea5762ac82f662ae2d85be1
sawaday <sawada.yusuke0108@gmail.com>
2015-08-25 12:17:14 +0900

    initial commit

---
+++ lib/yochiyochi_sawaday.rb
@@ -0,0 +1,38 @@
+require "yochiyochi_sawaday/version"
+
+class EnglishGame
+
+  def self.hello
+    puts "hello"
+  end 
+
+  def self.start
+    @words = { "apple"=> "りんご", "dog"=> "犬", "book"=> "本", "milk"=> "牛乳", "library"=> "図書館" }
+    @loop_count = 0
+    @correct_count = 0
+    puts "英単語ゲームスタート(5問)！stopと入力したら中断できます。"
+    @words.each do |key,value|
+      puts "#{value}は英語で？"
+
+      answer = gets.chomp
+
+      if answer == "stop"
+        puts "bye!" 
+        break
+      elsif answer == key
+        puts "正解です！" 
+        @correct_count+=1
+      else  
+        puts "不正解です。。正解は#{key}"
+      end   
+        @loop_count+=1
+    end     
+
+    if @loop_count==0
+      return 0
+    else  
+      puts "あなたの正解率は#{@correct_count*100/@loop_count}パーセントです"
+    end   
+
+  end 
+end   
---
+++ lib/yochiyochi_sawaday/version.rb
@@ -0,0 +1,3 @@
+module YochiyochiSawaday
+  VERSION = "0.1.0"
+end
